1c54b0ea8e8a19eaa5632d0eca03229ae3140c2e
Luu Quy Bao <nonalbrian95@gmail.com>
2025-11-26 19:46:37 +0700

    [REFACTOR] frontend

--- client/ev-coownership/src/api/groupService.js
+++
@@ -1,8 +0,0 @@
-import api from './api'
-
-export const groupService = {
-  getGroups: () => api.get('/groups').then(r => r.data),
-  getGroupById: (id) => api.get(`/groups/${id}`).then(r => r.data),
-  createBooking: (groupId, payload) => api.post(`/groups/${groupId}/bookings`, payload).then(r => r.data),
-  getBookings: (groupId, from, to) => api.get(`/groups/${groupId}/bookings`, { params: { from, to } }).then(r => r.data)
-}
--- client/ev-coownership/src/services/authService.js
+++
@@ -1,7 +0,0 @@
-import axios from "./axiosInstance";
-
-export const login = async (email, password) => {
-  const res = await axios.post("/auth/login", { email, password });
-  localStorage.setItem("token", res.data.token);
-  return res.data;
-};
