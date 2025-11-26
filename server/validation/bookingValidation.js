a0206975e160d9ea3fe75aa6d56d31ef815dcbff
Luu Quy Bao <nonalbrian95@gmail.com>
2025-11-26 14:59:55 +0700

    [REFACTOR] server side

--- server/validation/bookingValidation.js
+++
@@ -1,5 +0,0 @@
-exports.createBookingSchema = Joi3.object({
-  vehicleId: Joi3.string().required(),
-  startTime: Joi3.date().required(),
-  endTime: Joi3.date().required(),
-});
--- server/validation/paymentValidation.js
+++
@@ -1,5 +0,0 @@
-exports.addPaymentSchema = Joi3.object({
-  groupId: Joi3.string().required(),
-  amount: Joi3.number().positive().required(),
-  type: Joi3.string().valid("charging", "maintenance", "insurance", "cleaning"),
-});
